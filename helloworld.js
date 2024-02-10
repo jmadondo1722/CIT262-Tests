793cd3c5c5e25b24587a29f115f31c4b4eb758bd
jmadondo1722 <jmadondo1722@gmail.com>
2024-02-10 22:13:36 +0200

    Initial commit

---
+++ helloworld.js
@@ -0,0 +1,10 @@
+// helloworld.js
+
+// Function that returns your name
+function getName() {
+    return "Josiah";
+  }
+  
+  // Call the function and print "Hello" with the name
+  console.log("Hello", getName());
+  
